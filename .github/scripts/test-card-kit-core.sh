25c8011a5bf97ef0d8576b48a43c4e008a00d579
KorotkovAlex <alexkorotkovh@gmail.com>
2022-05-06 16:05:24 +0300

    Change ios version

--- .github/scripts/test-card-kit-core.sh
+++ .github/scripts/test-card-kit-core.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild -workspace CardKit.xcworkspace \
             -scheme CardKitCore \
-            -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 13 \
-            -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 13 \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
+            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
             clean test | xcpretty
