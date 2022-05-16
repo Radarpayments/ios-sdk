b7833bd33d1451b5e7c5760ebc040ffaa5b4d849
KorotkovAlex <alexkorotkovh@gmail.com>
2022-05-16 16:45:10 +0300

    Create CardKitPayment framework

---
+++ CardKit/CardKitPayment/CardKitPayment.h
@@ -0,0 +1,19 @@
+//
+//  CardKitPayment.h
+//  CardKitPayment
+//
+//  Created by Alex Korotkov on 16.05.2022.
+//  Copyright © 2022 AnjLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardKitPayment.
+FOUNDATION_EXPORT double CardKitPaymentVersionNumber;
+
+//! Project version string for CardKitPayment.
+FOUNDATION_EXPORT const unsigned char CardKitPaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardKitPayment/PublicHeader.h>
+
+
