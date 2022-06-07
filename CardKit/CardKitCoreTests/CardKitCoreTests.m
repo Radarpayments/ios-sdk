45dcaea01ce7277e5891c00a04d4125604d38473
Alex Korotkov <alexkorotkovh@gmail.com>
2022-06-07 16:55:09 +0300

    CardKitCore - change tests with mdorder

--- CardKit/CardKitCoreTests/CardKitCoreTests.m
+++ CardKit/CardKitCoreTests/CardKitCoreTests.m
@@ -28,14 +28,14 @@
     cardParams.pan=@"5536913776755304";
     cardParams.cvc = @"123";
     cardParams.mdOrder = @"mdorder";
-    cardParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp04PhwMu5k3fRMRmAb1ZRxbD3brU4a7oKa4NDlGXKQJiCEuw6e8SYcYW2i4rt0WsieeRRrrX7VnUZ2pH20lMtUnrTUtw2MaH5Ta9c3begST7sFkqU3t22BYedtamLGR5y55C5GWwI0Ie9ozecSckqcLW7KVITNT4GXME+Q1lFWYMGwr66vhu1fIV1pfVNWvMX3lEzVLwmwPkt0gf2ODR+AfO8rg17P4z4BHN/jSL0maOFsJlriCEf11jqtVbJKz5EDghyFO9Iw+gzorwlioc133li1OG0NbKzK/Nq5z29udoEWneisp3ub5M53jWvxDNiVl8uvPUfxyz+86mwNQ87QIDAQAB-----END PUBLIC KEY-----";
+    cardParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqcKSYD09AJLoqPaQGQ8Xt4KuT5kN5Utj2CM08gSZH1yzoTYejEkpi5ZlmQYNgpgXyBeF/qqhuTcvc1pcL/aBfpJgRrRTbLhMzvtIViv6OqmFaPQcBBcGBG2y8UzCuBgY9bqth4yw2b6jEmVN+wT7oRKcGcOU8SGt5yxi2VHJdb0hSnir+OPu9pA+fcQ4LqjZVhCQyPNk6NOp3hgrXou0KTVbpLg9UJexCvnj2AODK5NGvzqyBI7r86eHqlIM3SydHUT2DC+07+CXxFsCXR29tkKelUpX/itlkOMRM2jKeqVkbYHdVLcFmUFrxvW+N8ihRMK6AL/mbARHxPDiA8xtlwIDAQAB-----END PUBLIC KEY-----";
     
     
     bindingParams = [[CKCBindingParams alloc] init];
     bindingParams.bindingID = @"das";
     bindingParams.cvc = @"123";
     bindingParams.mdOrder = @"mdOrder";
-    bindingParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp04PhwMu5k3fRMRmAb1ZRxbD3brU4a7oKa4NDlGXKQJiCEuw6e8SYcYW2i4rt0WsieeRRrrX7VnUZ2pH20lMtUnrTUtw2MaH5Ta9c3begST7sFkqU3t22BYedtamLGR5y55C5GWwI0Ie9ozecSckqcLW7KVITNT4GXME+Q1lFWYMGwr66vhu1fIV1pfVNWvMX3lEzVLwmwPkt0gf2ODR+AfO8rg17P4z4BHN/jSL0maOFsJlriCEf11jqtVbJKz5EDghyFO9Iw+gzorwlioc133li1OG0NbKzK/Nq5z29udoEWneisp3ub5M53jWvxDNiVl8uvPUfxyz+86mwNQ87QIDAQAB-----END PUBLIC KEY-----";
+    bindingParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqcKSYD09AJLoqPaQGQ8Xt4KuT5kN5Utj2CM08gSZH1yzoTYejEkpi5ZlmQYNgpgXyBeF/qqhuTcvc1pcL/aBfpJgRrRTbLhMzvtIViv6OqmFaPQcBBcGBG2y8UzCuBgY9bqth4yw2b6jEmVN+wT7oRKcGcOU8SGt5yxi2VHJdb0hSnir+OPu9pA+fcQ4LqjZVhCQyPNk6NOp3hgrXou0KTVbpLg9UJexCvnj2AODK5NGvzqyBI7r86eHqlIM3SydHUT2DC+07+CXxFsCXR29tkKelUpX/itlkOMRM2jKeqVkbYHdVLcFmUFrxvW+N8ihRMK6AL/mbARHxPDiA8xtlwIDAQAB-----END PUBLIC KEY-----";
 }
 
 - (void)tearDown {
@@ -228,24 +228,15 @@
     XCTAssertEqualObjects(res.errors[0], expectedResponse);
 }
 
-- (void)testGenerateTokenWithCardWithoutMdOrder {
-    cardParams.mdOrder =@"";
-    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
-    NSDictionary *expectedResponse = @{@"field": CKCFieldMdOrder, @"error": CKCErrorRequired};
-
-    XCTAssertEqualObjects(res.errors[0], expectedResponse);
-}
-
 - (void)testGenerateTokenWithCardWithNilMdOrder {
     cardParams.mdOrder = nil;
     CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
-    NSDictionary *expectedResponse = @{@"field": CKCFieldMdOrder, @"error": CKCErrorRequired};
 
-    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+    XCTAssertNotNil(res.token, @"pointer:%p", res.token);
 }
 
 - (void)testReturnPubKeyFromJSONString {
-    NSString *pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhjH8R0jfvvEJwAHRhJi2Q4fLi1p2z10PaDMIhHbD3fp4OqypWaE7p6n6EHig9qnwC/4U7hCiOCqY6uYtgEoDHfbNA87/X0jV8UI522WjQH7Rgkmgk35r75G5m4cYeF6OvCHmAJ9ltaFsLBdr+pK6vKz/3AzwAc/5a6QcO/vR3PHnhE/qU2FOU3Vd8OYN2qcw4TFvitXY2H6YdTNF4YmlFtj4CqQoPL1u/uI0UpsG3/epWMOk44FBlXoZ7KNmJU29xbuiNEm1SWRJS2URMcUxAdUfhzQ2+Z4F0eSo2/cxwlkNA+gZcXnLbEWIfYYvASKpdXBIzgncMBro424z/KUr3QIDAQAB-----END PUBLIC KEY-----";
+    NSString *pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqcKSYD09AJLoqPaQGQ8Xt4KuT5kN5Utj2CM08gSZH1yzoTYejEkpi5ZlmQYNgpgXyBeF/qqhuTcvc1pcL/aBfpJgRrRTbLhMzvtIViv6OqmFaPQcBBcGBG2y8UzCuBgY9bqth4yw2b6jEmVN+wT7oRKcGcOU8SGt5yxi2VHJdb0hSnir+OPu9pA+fcQ4LqjZVhCQyPNk6NOp3hgrXou0KTVbpLg9UJexCvnj2AODK5NGvzqyBI7r86eHqlIM3SydHUT2DC+07+CXxFsCXR29tkKelUpX/itlkOMRM2jKeqVkbYHdVLcFmUFrxvW+N8ihRMK6AL/mbARHxPDiA8xtlwIDAQAB-----END PUBLIC KEY-----";
     NSString *jsonString = @"{\"keys\":[{\"keyValue\":\"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhjH8R0jfvvEJwAHRhJi2Q4fLi1p2z10PaDMIhHbD3fp4OqypWaE7p6n6EHig9qnwC/4U7hCiOCqY6uYtgEoDHfbNA87/X0jV8UI522WjQH7Rgkmgk35r75G5m4cYeF6OvCHmAJ9ltaFsLBdr+pK6vKz/3AzwAc/5a6QcO/vR3PHnhE/qU2FOU3Vd8OYN2qcw4TFvitXY2H6YdTNF4YmlFtj4CqQoPL1u/uI0UpsG3/epWMOk44FBlXoZ7KNmJU29xbuiNEm1SWRJS2URMcUxAdUfhzQ2+Z4F0eSo2/cxwlkNA+gZcXnLbEWIfYYvASKpdXBIzgncMBro424z/KUr3QIDAQAB-----END PUBLIC KEY-----\",\"protocolVersion\":\"RSA\",\"keyExpiration\":1661599747000}]}";
 
     NSString *result = [CKCPubKey fromJSONString: jsonString];
