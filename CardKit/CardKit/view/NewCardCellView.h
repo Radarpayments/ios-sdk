8c39c2f3059b6d3a93fc709d7c18f7b6250ab79a
Alex Korotkov <alexkorotkovh@gmail.com>
2022-07-21 21:46:20 +0300

    Create new card cell

---
+++ CardKit/CardKit/view/NewCardCellView.h
@@ -0,0 +1,17 @@
+//
+//  NewCardCellView.h
+//  CardKit
+//
+//  Created by Alex Korotkov on 17.07.2022.
+//  Copyright © 2022 AnjLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewCardCellView: UIView
+- (UIImage *) imagePath;
+- (NSString *) label;
+@end
+
+NS_ASSUME_NONNULL_END
