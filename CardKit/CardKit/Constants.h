81f00fe5bceeb5ea5b7b2812ba6f2638b41046f1
Alex Korotkov <alexkorotkovh@gmail.com>
2022-07-21 21:39:17 +0300

    Add Constants

---
+++ CardKit/CardKit/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  CardKit
+//
+//  Created by Alex Korotkov on 17.07.2022.
+//  Copyright © 2022 AnjLab. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+extern NSString* const CardKSavedCardsCellID;
+extern NSString* const CardKPayCardButtonCellID;
+extern NSString* const CardKApplePayButtonCellID;
+extern NSString* const CardKDividerCellID;
+extern NSString* const NewCardCellID;
+extern NSString* const AllPaymentMethodsCellID;
+extern NSString* const CardKKindPayRows;
---
+++ CardKit/CardKit/Constants.m
@@ -0,0 +1,16 @@
+//
+//  Constants.m
+//  CardKit
+//
+//  Created by Alex Korotkov on 17.07.2022.
+//  Copyright © 2022 AnjLab. All rights reserved.
+//
+
+#import "Constants.h"
+NSString* const CardKSavedCardsCellID = @"savedCards";
+NSString* const CardKPayCardButtonCellID = @"button";
+NSString* const CardKApplePayButtonCellID = @"applePay";
+NSString* const CardKDividerCellID = @"divider";
+NSString* const NewCardCellID = @"newCard";
+NSString* const AllPaymentMethodsCellID = @"allPaymentMethods";
+NSString* const CardKKindPayRows = @"rows";
